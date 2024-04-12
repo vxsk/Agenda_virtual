441d9f7cea0e2c58c9c2e064b39aa1af2aaaf29a
vxsk <felipe_adomi@hotmail.com>
2024-04-12 09:50:36 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('form-agenda');
+const contatos = [];
+const telefones = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela()
+
+})
+
+function adicionaLinha(){ 
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputTelefoneContato = document.getElementById('telefone-contato');
+
+    if (contatos.includes(inputNomeContato.ariaValueMax)){
+        alert(`Atenção! ${inputNomeContato} já existe na lista de contatos! `);
+    }else {
+        contatos.push(inputNomeContato.value);
+        telefones.push(inputTelefoneContato);
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputTelefoneContato.value}</td>`;
+        linha += `</tr>`;
+        linhas += linha;
+    }
+
+    inputNomeContato.value = '';
+    inputTelefoneContato.value = '';
+};
+
+function atualizaTabela(){ 
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
